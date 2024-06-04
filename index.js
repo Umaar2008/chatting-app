895062d65ad9eaecfb82c9a2a4bd988625e5e3f5
user.name : umaarkhan <user.email : umaaarkhan2008@gmail.com>
2024-06-04 19:32:37 +0500

    ll

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const server = http.createServer(app)
 
 const io = new Server(server)
 //socket io
+app.get('/' , (req , res)=>{
+    res.sendFile(__dirname + '/index.html')
+})
 
 io.on('connection' , (socket)=>{
 socket.on('user-message' , (message)=> {
